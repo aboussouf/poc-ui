c4fddfc3368f3386e7767a97187e599850279cad
Ali Bou <boussouf141@gmail.com>
2022-05-03 22:28:23 +0200

    feature/app: add text on app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,112 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Counter } from './app/coreLogic/features/counter/Counter';
+import React from "react";
+import {
+  MantineProvider,
+  createStyles,
+  Title,
+  SimpleGrid,
+  Text,
+  Button,
+  ThemeIcon,
+  Grid,
+  Col,
+} from "@mantine/core";
+import { ReceiptOff, Flame, CircleDotted, FileCode } from "tabler-icons-react";
+
+const useStyles = createStyles((theme) => ({
+  wrapper: {
+    padding: `${theme.spacing.xl * 2}px ${theme.spacing.xl}px`,
+  },
+
+  title: {
+    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+    fontSize: 36,
+    fontWeight: 900,
+    lineHeight: 1.1,
+    marginBottom: theme.spacing.md,
+    color: theme.colorScheme === "dark" ? theme.white : theme.black,
+  },
+}));
+
+const features = [
+  {
+    icon: ReceiptOff,
+    title: "LinkedIn profile",
+    description:
+      "You can view in brief my projects in this profile, to have an overview of my work.",
+  },
+  {
+    icon: FileCode,
+    title: "My CV(TODO)",
+    description:
+      "My experience in detail, and my skills in detail, in a single document.",
+  },
+  {
+    icon: CircleDotted,
+    title: "Languages",
+    description: "I speak English, French.",
+  },
+  {
+    icon: Flame,
+    title: "Hobbies",
+    description: "TODO",
+  },
+];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+  const { classes } = useStyles();
+
+  const items = features.map((feature) => (
+    <div key={feature.title}>
+      <ThemeIcon
+        size={44}
+        radius="md"
+        variant="gradient"
+        gradient={{ deg: 133, from: "blue", to: "cyan" }}
+      >
+        <feature.icon size={26} />
+      </ThemeIcon>
+      <Text size="lg" mt="sm" weight={500}>
+        {feature.title}
+      </Text>
+      <Text color="dimmed" size="sm">
+        {feature.description}
+      </Text>
     </div>
+  ));
+  return (
+    <MantineProvider theme={{ colorScheme: "dark" }}>
+      <div className={classes.wrapper}>
+        <Grid gutter={80}>
+          <Col span={12} md={5}>
+            <Title className={classes.title} order={2}>
+              Project to present my experiences
+            </Title>
+            <Text color="dimmed">
+              Check options in right side to see my profiles throw the net
+            </Text>
+
+            <Button
+              variant="gradient"
+              gradient={{ deg: 133, from: "blue", to: "cyan" }}
+              size="lg"
+              radius="md"
+              mt="xl"
+            >
+              Get started
+            </Button>
+          </Col>
+          <Col span={12} md={7}>
+            <SimpleGrid
+              cols={2}
+              spacing={30}
+              breakpoints={[{ maxWidth: "md", cols: 1 }]}
+            >
+              {items}
+            </SimpleGrid>
+          </Col>
+        </Grid>
+      </div>
+    </MantineProvider>
   );
 }
 
